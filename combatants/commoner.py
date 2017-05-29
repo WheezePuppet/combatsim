3eb75bd1f61d2213c75e8f75f580597434ef2653
Stephen <stephen@umw.edu>
2017-05-29 13:51:34 -0400

    Rearrange properties. Add initiative.

--- combatants/commoner.py
+++ combatants/commoner.py
@@ -1,12 +1,13 @@
 {
-    "int": 10,
     "name": "commoner",
-    "con": 10,
-    "hp": "1d6-2",
-    "dex": 10,
     "str": 10,
-    "cha": 10,
+    "dex": 10,
+    "int": 10,
     "wis": 10,
+    "con": 10,
+    "cha": 10,
     "ac": 6,
+    "hp": "1d6-2",
+    "initiative": "d20",
     "action_strs": [ "MeleeAttack(mod=0,reach=5,damage='d4-1')" ]
 }
--- combatants/kobold.py
+++ combatants/kobold.py
@@ -1,12 +1,13 @@
 {
-    "int": 9,
     "name": "kobold",
-    "con": 7,
-    "hp": "2d6-2",
-    "dex": 15,
     "str": 7,
-    "cha": 8,
+    "dex": 15,
+    "int": 9,
     "wis": 8,
+    "con": 7,
+    "cha": 8,
     "ac": 10,
+    "hp": "2d6-2",
+    "initiative": "d20+2",
     "action_strs": [ "MeleeAttack(mod=2,reach=5,damage='d6-1')" ]
 }
