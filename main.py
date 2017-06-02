49da2fca2ae75c97fd88d41ed6bf305ac1f302c1
Stephen <stephen@umw.edu>
2017-06-01 21:24:42 -0400

    Change command-line arguments. Allow auto-plot.

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 
 import logging
 import sys
+import os
 
 from SimCombat import *
 from ParameterSweep import run_sample_sweep
 
-if len(sys.argv) > 3:
-    print('Usage: main.py [(' + '|'.join(reversed(logging_levels)) +
-        ')] [suite_size].')
+if len(sys.argv) > 4:
+    print('Usage: main.py [suite_size=1] [(' +
+        '|'.join(reversed(logging_levels)) + 
+        ')=DEBUG] [plot=True].')
     sys.exit(1)
 
 if len(sys.argv) > 1:
-    logging.getLogger().setLevel(sys.argv[1])
-else:
-    logging.getLogger().setLevel('DEBUG')
-
-if len(sys.argv) > 2:
-    suite_size=int(sys.argv[2])
+    suite_size=int(sys.argv[1])
 else:
     suite_size=1
 
+if len(sys.argv) > 2:
+    logging.getLogger().setLevel(sys.argv[2])
+else:
+    logging.getLogger().setLevel('DEBUG')
+
+if len(sys.argv) > 3:
+    plot = True if sys.argv[3] == 'True' else False
+else:
+    plot = True
+
 with open('results.csv','w') as f:
     run_sample_sweep(suite_size).to_csv(f, index=False)
+if plot:
+    os.system('Rscript plotParamSweep.R')
+    os.system('evince plot.pdf &')
