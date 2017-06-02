87deecc0a8fcf07aa40a3184d1be7dca0019f323
Stephen <stephen@umw.edu>
2017-06-01 21:25:04 -0400

    Enhance plot captions. Save to PDF.

--- plotParamSweep.R
+++ plotParamSweep.R
@@ -4,10 +4,20 @@ require(stringr)
 
 r <- read.csv("results.csv")
 
-r$val <- as.numeric(substr(r$val,str_locate(pattern="\\+",r$val)+1,length(r$val)))
-p <- ggplot(r, aes(x=val, y=party_win_freq)) + geom_point() + geom_smooth() +
-    ggtitle("How buff is enough?")  +
-    xlab("Buff commoner's damage modifier")  +
-    ylab("Party win %") 
+if (any(str_detect(r$value,"\\+"))) {
+    r$value <- as.numeric(
+        substr(r$value,str_locate(pattern="\\+",r$value)+1,length(r$value)))
+    text <- "modifier"
+} else {
+    text <- "value"
+}
 
-print(p)
+p <- ggplot(r, aes(x=value, y=party_win_freq)) + geom_point() + geom_smooth() +
+    ggtitle(paste0(r$combatant,"'s ",r$param, " ", text))  +
+    xlab(paste(r$param, text))  +
+    ylab("Party win %")  +
+    ylim(c(0,1))
+
+p <- p + geom_hline(yintercept=.5, color="red")
+
+ggsave("plot.pdf",p)
