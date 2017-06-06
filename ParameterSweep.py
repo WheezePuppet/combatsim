5df2d9a5b52b476506b9f4c2a28edf79b4ea6c7b
Stephen <stephen@umw.edu>
2017-06-06 14:29:38 -0400

    Hydrate ParameterSweep from file.

--- ParameterSweep.py
+++ ParameterSweep.py
@@ -1,6 +1,7 @@
 
 from random import randint
 import pandas as pd
+import re
 
 from Suite import Suite
 from Combatant import Combatant
@@ -25,6 +26,35 @@ class ParameterSweep():
         self._encounter = encounter
         self._sweep_params = sweep_params
 
+
+    @classmethod
+    def from_filename(self, filename):
+        '''Hydrate a ParameterSweep object from a file. The file should be in
+        this format:
+
+        --------------------------------------------------------------------
+        Party:
+        commoner,3,hp:d6+(9-50)
+
+        Monsters:
+        kobold,4
+        hydra,2
+        --------------------------------------------------------------------
+        If present after the quantity, sweep parameters with ranges are
+        defined for that combatant.
+        '''
+        with open(filename, "r") as f:
+            lines = [line.rstrip() for line in f.readlines() 
+                if len(line.strip()) > 0]
+            party_lines = lines[1:lines.index('Monsters:')]
+            monsters_lines = lines[lines.index('Monsters:')+1:]
+
+            party, sweep_params = instantiate_group(party_lines)
+            monsters, sweep_params2 = instantiate_group(monsters_lines)
+
+            sweep_params.update(sweep_params2)
+
+        return ParameterSweep(Encounter(party,monsters),sweep_params)
 
     def execute(self, suite_size=10):
 
@@ -52,6 +82,36 @@ class ParameterSweep():
                         'value':val,
                         'party_win_freq':party_win_freq}]))
         return results[['combatant','param','value','party_win_freq']]
+
+
+def instantiate_group(lines):
+    group = []
+    sweep_params = {}
+    for combatant in lines:
+        combatant_parts = combatant.split(',')
+        these_combatants = [
+            Combatant.from_filename(combatant_parts[0])
+                for _ in range(int(combatant_parts[1]))]
+        group.extend(these_combatants)
+        if len(combatant_parts) > 2:
+            for param in combatant_parts[2:]:
+                param_parts = param.split(':')
+                sweep_params.update(
+                    {tuple(c for c in these_combatants):{
+                    param_parts[0]:build_pv_list(param_parts[1])}})
+    return group, sweep_params
+
+
+def build_pv_list(string):
+    '''From a string like 'd6+(0-5)', return a list incarnating all the
+    values in parens. In this case, the list would be ['d6+0','d6+1','d6+2',
+    'd6+3','d6+4','d6+5'].'''
+    if '(' not in string:
+        return [ string ]
+    prefix = string[0:string.index('(')]
+    first_num = int(string[string.index('(')+1:string.index('-')])
+    last_num = int(string[string.index('-')+1:string.index(')')])
+    return [ prefix + str(k) for k in range(first_num,last_num+1) ]
 
 
 def run_sample_sweep(suite_size=10):
--- main.py
+++ main.py
@@ -4,31 +4,42 @@ import sys
 import os
 
 from SimCombat import *
-from ParameterSweep import run_sample_sweep
+from ParameterSweep import ParameterSweep
 
 if len(sys.argv) > 4:
-    print('Usage: main.py [suite_size=1] [(' +
-        '|'.join(reversed(logging_levels)) + 
-        ')=DEBUG] [plot=True].')
+    print('Usage: main.py [sweep_file=sweep.txt] [suite_size=1] [(' +
+        '|'.join(reversed(logging_levels)) +
+        ')=DEBUG] [plot=False].')
     sys.exit(1)
 
 if len(sys.argv) > 1:
-    suite_size=int(sys.argv[1])
+    enc_file = (
+        sys.argv[1].split('=')[1] if sys.argv[1].startswith('sweep_file') 
+        else sys.argv[1])
+else:
+    enc_file = 'sweep.txt'
+
+if len(sys.argv) > 2:
+    suite_size = (
+        sys.argv[2].split('=')[1] if sys.argv[2].startswith('suite_size') 
+        else sys.argv[2])
+    suite_size=int(suite_size)
 else:
     suite_size=1
 
-if len(sys.argv) > 2:
-    logging.getLogger().setLevel(sys.argv[2])
+if len(sys.argv) > 3:
+    logging.getLogger().setLevel(sys.argv[3])
 else:
     logging.getLogger().setLevel('DEBUG')
 
-if len(sys.argv) > 3:
-    plot = True if sys.argv[3] == 'True' else False
+if len(sys.argv) > 4:
+    plot = False if sys.argv[4] in ['False', 'plot=False'] else True
 else:
-    plot = True
+    plot = False
 
 with open('results.csv','w') as f:
-    run_sample_sweep(suite_size).to_csv(f, index=False)
+    results = ParameterSweep.from_filename(enc_file).execute(suite_size)
+    results.to_csv(f, index=False)
 if plot:
     os.system('Rscript plotParamSweep.R')
     os.system('evince plot.pdf &')
