019b95d454003a7b901929242d211dda6faf2092
Stephen <stephen@umw.edu>
2017-06-15 17:23:09 -0400

    Use "encounter file" not "sweep file" for GUI.
    An encounter file is just a sweep file with no varying parameters in it. For
    clarity, call it "encounter.txt" by default and change visual.py terminology.

--- visual.py
+++ visual.py
@@ -7,7 +7,7 @@ from SimCombat import *
 from ParameterSweep import ParameterSweep
 
 if len(sys.argv) > 3:
-    print('Usage: visual.py [enc_file=sweep.txt] [(' +
+    print('Usage: visual.py [enc_file=encounter.txt] [(' +
         '|'.join(reversed(logging_levels)) + ')=METADETAIL].')
     sys.exit(1)
 
@@ -16,12 +16,14 @@ if len(sys.argv) > 1:
         sys.argv[1].split('=')[1] if sys.argv[1].startswith('enc_file') 
         else sys.argv[1])
 else:
-    enc_file = 'sweep.txt'
+    enc_file = 'encounter.txt'
 
 if len(sys.argv) > 2:
     logging.getLogger().setLevel(sys.argv[3])
 else:
     logging.getLogger().setLevel('METADETAIL')
 
-results = ParameterSweep.from_filename(enc_file).execute(1)
+really_not_a_sweep = ParameterSweep.from_filename(enc_file)
+encounter = really_not_a_sweep._encounter
+results = encounter.simulate()
 print(results)
