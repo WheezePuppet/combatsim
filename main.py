8013b48ad9e5ba049d601d43908a303a47371f4b
Stephen <stephen@umw.edu>
2017-06-15 17:11:23 -0400

    Tweak command-line args and variable names.

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ from ParameterSweep import ParameterSweep
 if len(sys.argv) > 5:
     print('Usage: main.py [sweep_file=sweep.txt] [suite_size=1] [(' +
         '|'.join(reversed(logging_levels)) +
-        ')=DEBUG] [plot=False].')
+        ')=METADETAIL] [plot=False].')
     sys.exit(1)
 
 if len(sys.argv) > 1:
-    enc_file = (
+    sweep_file = (
         sys.argv[1].split('=')[1] if sys.argv[1].startswith('sweep_file') 
         else sys.argv[1])
 else:
-    enc_file = 'sweep.txt'
+    sweep_file = 'sweep.txt'
 
 if len(sys.argv) > 2:
     suite_size = (
@@ -30,7 +30,7 @@ else:
 if len(sys.argv) > 3:
     logging.getLogger().setLevel(sys.argv[3])
 else:
-    logging.getLogger().setLevel('DEBUG')
+    logging.getLogger().setLevel('METADETAIL')
 
 if len(sys.argv) > 4:
     plot = False if sys.argv[4] in ['False', 'plot=False'] else True
@@ -38,7 +38,7 @@ else:
     plot = False
 
 with open('results.csv','w') as f:
-    results = ParameterSweep.from_filename(enc_file).execute(suite_size)
+    results = ParameterSweep.from_filename(sweep_file).execute(suite_size)
     results.to_csv(f, index=False)
 if plot:
     os.system('Rscript plotParamSweep.R')
